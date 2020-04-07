ce4d4eff7b53c49319b30e07193e5a86a669561c
esmyers <emyers@usgs.gov>
2020-04-07 14:51:51 -0500

    changes before pull

--- src/app/mainview/mainview.module.ts
+++ src/app/mainview/mainview.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MapComponent } from './map/map.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { SharedModule } from '../shared/shared.module';
@@ -11,7 +12,7 @@ import { DataviewComponent } from './dataview/dataview.component';
 
 @NgModule({
   declarations: [MapComponent, SidebarComponent, DataviewComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, SharedModule],
   exports: [MapComponent, SidebarComponent, DataviewComponent]
 })
 export class MainviewModule { }
