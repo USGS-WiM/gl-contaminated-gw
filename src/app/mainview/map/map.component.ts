ec73fcd56305c2a2fba1b34f5546305afbd59b79
Myers <EMYERS@USGS.GOV>
2020-12-17 15:20:13 -0600

    cleans up auxLayer switching, adds aux hydro layer

--- src/app/mainview/map/map.component.ts
+++ src/app/mainview/map/map.component.ts
@@ -1,105 +1,104 @@
-import { Component, OnInit } from '@angular/core';
-import ontarioJSON from '../../../assets/OntarioClipped_9.26.json'; //https://github.com/angular/angular/issues/30802  //https://stackoverflow.com/questions/46991237/how-to-import-json-file-into-a-typescript-file
-import basins_2k from './basin_simple_2km.json';
+import { Component, OnInit } from "@angular/core";
+import ontarioJSON from "../../../assets/OntarioClipped_9.26.json"; //https://github.com/angular/angular/issues/30802  //https://stackoverflow.com/questions/46991237/how-to-import-json-file-into-a-typescript-file
+import basins_2k from "./basin_simple_2km.json";
 // tslint:disable-next-line: max-line-length
-import {MapService} from '../../shared/services/map.service'
-import {Map} from 'leaflet';
-import * as L from 'leaflet';
-import * as esri from 'esri-leaflet';
+import { MapService } from "../../shared/services/map.service";
+import { Map } from "leaflet";
+import * as L from "leaflet";
+import * as esri from "esri-leaflet";
 
 @Component({
-  selector: 'app-mainview-map',
-  templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+  selector: "app-mainview-map",
+  templateUrl: "./map.component.html",
+  styleUrls: ["./map.component.scss"],
 })
 export class MapComponent implements OnInit {
   collapsedMap;
   collapsedDataPanel;
   legendExpanded = true;
 
-  constructor(private _mapService: MapService) { }
+  constructor(private _mapService: MapService) {}
 
   ngOnInit() {
-
-   
-
     // let canadaSites = this._mapService.getCanadaData(ontarioJSON);
-    
-    this._mapService.map = L.map('map', {
+
+    this._mapService.map = L.map("map", {
       center: L.latLng(43.3, -84.6),
       zoom: 5,
-      renderer: L.canvas()
+      renderer: L.canvas(),
     });
-    this._mapService.QueryUSData();
-    this._mapService.QueryCanadaData();
+
     //keeps the geojson always on the top of all other layers
-    this._mapService.map.createPane('basins');
-    //this._mapService.map.getPane('basins').style.zIndex = '1';
-    this._mapService.map.createPane('sites');
-    this._mapService.map.addLayer(this._mapService.baseMaps[this._mapService.chosenBaseLayer]);
-    
-    
-    this._mapService.sitesLayer = L.featureGroup().addTo(this._mapService.map)
-    //usSites.addTo(this._mapService.sitesLayer);
+    this._mapService.map.createPane("basins");
+    //this._mapService.map.getPane("basins").style.zIndex = "1";
+    this._mapService.map.createPane("sites");
+    this._mapService.map.addLayer(
+      this._mapService.baseMaps[this._mapService.chosenBaseLayer]
+    );
 
-    //canSites.addTo(this._mapService.sitesLayer);
-    // canadaSites.addTo(this._mapService.sitesLayer);
+    this._mapService.defaultAuxLayers.forEach((layerName) =>
+      this._mapService.map.addLayer(this._mapService.auxLayers[layerName])
+    );
+
+    this._mapService.sitesLayer = L.featureGroup().addTo(this._mapService.map);
     this._mapService.map.addLayer(this._mapService.sitesLayer);
-    this._mapService.map.addLayer(this._mapService.auxLayers['basinArea']);
 
-    this._mapService.legend = new L.Control({position: 'bottomright'});
+    this._mapService.legend = new L.Control({ position: "bottomright" });
 
-    this._mapService.legend.onAdd = function (map){
-      const div = L.DomUtil.create("div", 'info legend');
+    this._mapService.legend.onAdd = function (map) {
+      const div = L.DomUtil.create("div", "info legend");
       let item = "";
 
-      item += '<div id="LegendHeader" ><span><i class="fa fa-list"></i>Explanation</span></div>'+
+      item +=
+        '<div id="LegendHeader" ><span><i class="fa fa-list"></i>Explanation</span></div>' +
         '<div id="legendDiv"><br>';
-           item += '<i class="site multiple-types"></i>Multiple</div>';
-           div.innerHTML = item;
-           div.id = 'legend';
+      item += '<i class="site multiple-types"></i>Multiple</div>';
+      div.innerHTML = item;
+      div.id = "legend";
 
-           L.DomEvent.on(div, 'click', (event) => {
-            // if click is in Explanation title, collapse/expand it.
-            const id = event.target['id'];
-            if ('legendHeader') {
-                const classes = document.getElementById('legendDiv').classList;
-                if (classes.contains('legendDiv-collapsed')) {
-                    classes.remove('legendDiv-collapsed');
-                } else {
-                    classes.add('legendDiv-collapsed');
-                }
-            }
-          });
-          return div;
-    }
+      L.DomEvent.on(div, "click", (event) => {
+        // if click is in Explanation title, collapse/expand it.
+        const id = event.target["id"];
+        if ("legendHeader") {
+          const classes = document.getElementById("legendDiv").classList;
+          if (classes.contains("legendDiv-collapsed")) {
+            classes.remove("legendDiv-collapsed");
+          } else {
+            classes.add("legendDiv-collapsed");
+          }
+        }
+      });
+      return div;
+    };
     this._mapService.legend.addTo(this._mapService.map);
 
-    this._mapService.DataPanelCollapse.subscribe(collapse => {
+    this._mapService.DataPanelCollapse.subscribe((collapse) => {
       this.collapsedDataPanel = collapse;
     });
 
-    
+    this.expandCollapseDataPanel();
+
     //let usSites = this._mapService.getUSsiteData();
 
     //add empty feature group
     //this.sitesLayer = L.featureGroup().addTo(this._mapService.map);
-    
+
     //keeps the geojson always on the top of all other layers
     /* this.map.createPane('basins');
     this.map.createPane('geojson'); */
-
-    
   }
 
   expandCollapseDataPanel() {
     this._mapService.dataPanelCollapseSubject.next(!this.collapsedDataPanel);
-  }
-
-  resizeMap() {
     setTimeout(() => {
-        this._mapService.map.invalidateSize()
+      this._mapService.map.invalidateSize();
     }, 150);
   }
 
+  //could probably be delete but I don't know if any other code calls this; expecially in the component template
+  resizeMap() {
+    setTimeout(() => {
+      this._mapService.map.invalidateSize();
+    }, 150);
+  }
 }
--- src/app/mainview/sidebar/sidebar.component.ts
+++ src/app/mainview/sidebar/sidebar.component.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit, Directive, Input } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { MapService } from 'src/app/shared/services/map.service';
-import { ConvertPropertyBindingResult } from '@angular/compiler/src/compiler_util/expression_converter';
-import { PromiseType } from 'protractor/built/plugins';
-import { BoundDirectivePropertyAst } from '@angular/compiler';
+import { Component, OnInit, Directive, Input } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { MapService } from "src/app/shared/services/map.service";
+import { ConvertPropertyBindingResult } from "@angular/compiler/src/compiler_util/expression_converter";
+import { PromiseType } from "protractor/built/plugins";
+import { BoundDirectivePropertyAst } from "@angular/compiler";
 
 @Component({
-  selector: 'app-mainview-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  selector: "app-mainview-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.scss"],
 })
 export class SidebarComponent implements OnInit {
   siteFilterFormGroup: FormGroup;
   siteFilterData;
   filterSelections;
-  filterOptions = ['EPA Region', 'Site Type'];
+  filterOptions = ["EPA Region", "Site Type"];
   expandSidebar;
   showBasemaps;
   showAuxLayers;
@@ -23,31 +23,31 @@ export class SidebarComponent implements OnInit {
   displayedAuxLayers;
   profileForm: FormGroup;
 
-  constructor(private _mapService: MapService, private _formBuilder: FormBuilder) {}
+  constructor(
+    private _mapService: MapService,
+    private _formBuilder: FormBuilder
+  ) {}
 
   ngOnInit() {
-    this.displayedAuxLayers = [];
-    
+    this.displayedAuxLayers = this._mapService.defaultAuxLayers;
 
     /*     this.profileForm = this._formBuilder.group({
       firstName: [''],
       lastName: [''],
     }) */
-    this.siteFilterData = ['a', 'b']
+    this.siteFilterData = ["a", "b"];
     this.siteFilterFormGroup = this._formBuilder.group({
       //epa region location
       location: [[]],
       //map symbol code
-      siteType: [[]]
+      siteType: [[]],
       //orgName: [[]],
       //provider: [[]],
       //searchType: [[]],
       //type: [[]]
-  });
+    });
 
-
-
-/*     this._mapService.getUSsiteData().subscribe(response =>{
+    /*     this._mapService.getUSsiteData().subscribe(response =>{
       this.siteFilterData = response;
     }) */
 
@@ -56,32 +56,31 @@ export class SidebarComponent implements OnInit {
   // called from basemap button click in sidebar
   public toggleLayer(newVal: string) {
     this._mapService.chosenBaseLayer = newVal;
-    this._mapService.map.removeLayer(this._mapService.baseMaps['OpenStreetMap']);
-    this._mapService.map.removeLayer(this._mapService.baseMaps['Topo']);
-    this._mapService.map.removeLayer(this._mapService.baseMaps['Terrain']);
-    this._mapService.map.removeLayer(this._mapService.baseMaps['Satellite']);
-    this._mapService.map.removeLayer(this._mapService.baseMaps['Gray']);
-    this._mapService.map.removeLayer(this._mapService.baseMaps['Nautical']);
+    this._mapService.map.removeLayer(
+      this._mapService.baseMaps["OpenStreetMap"]
+    );
+    this._mapService.map.removeLayer(this._mapService.baseMaps["Topo"]);
+    this._mapService.map.removeLayer(this._mapService.baseMaps["Terrain"]);
+    this._mapService.map.removeLayer(this._mapService.baseMaps["Satellite"]);
+    this._mapService.map.removeLayer(this._mapService.baseMaps["Gray"]);
+    this._mapService.map.removeLayer(this._mapService.baseMaps["Nautical"]);
     this._mapService.map.addLayer(this._mapService.baseMaps[newVal]);
   }
 
-  public toggleAuxLayer(newVal: string){
+  public toggleAuxLayer(newVal: string) {
     let index = this.displayedAuxLayers.indexOf(newVal);
-    if (index > -1){
-      this.displayedAuxLayers.splice(index,1);
+    if (index == -1) {
+      this.displayedAuxLayers.push(newVal);
       this._mapService.map.addLayer(this._mapService.auxLayers[newVal]);
     } else {
-      this.displayedAuxLayers.push(newVal);
+      this.displayedAuxLayers.splice(index, 1);
       this._mapService.map.removeLayer(this._mapService.auxLayers[newVal]);
     }
   }
 
-   // public onChanges(): void {
+  // public onChanges(): void {
   //   this.siteFilterFormGroup.valueChanges.subscribe(selections => {
   //     this.filterSelections = selections;
   //   });
   // }
-
-
-
 }
